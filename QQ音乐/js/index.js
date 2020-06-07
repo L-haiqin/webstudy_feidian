f64e1947efe3a5bf1b4f9c62f1d756401f614ef3
system-bit <1448851375@qq.com>
2020-06-07 21:29:33 +0800

    歌单推荐

---
+++ QQ音乐/js/index.js
@@ -0,0 +1,12 @@
+window.addEventListener('load',function () {
+    //1.获取元素
+    // var leftBtn = document.querySelector('.leftBtn');
+    // var rightBtn = document.querySelector('.rightBtn');
+    // var playlist = document.querySelector('.playlist');
+    //
+    // playlist.addEventListener('mouseenter',function () {
+    //     leftBtn.style.display = 'block';
+    //     rightBtn.style.display = 'block';
+    // })
+
+} )
